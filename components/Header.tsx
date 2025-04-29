c5e6d410e6f786215c753fc410f58ac8cd33b3e5
Jon Perry <jonperry113@gmail.com>
2025-04-29 18:02:22 -0400

    Build Header component

---
+++ components/Header.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+import Image from "next/image";
+
+import { Button } from "./ui/button";
+import ReddishLogo from "@/images/Reddish Full.png";
+import ReddishLogoOnly from "@/images/Reddish Logo Only.png";
+import { MenuIcon } from "lucide-react";
+
+function Header() {
+  const { isSignedIn, user } = useUser();
+
+  const toggleSidebar = () => {};
+
+  return (
+    <header className="flex items-center justify-between p-4 border-b border-b-gray-200">
+      {/* Left side */}
+      <div className="flex items-center gap-2">
+        <MenuIcon className="w-6 h-6" onClick={toggleSidebar} />
+        <Image
+          src={ReddishLogo}
+          alt="logo"
+          width={150}
+          height={150}
+          className="hidden md:block"
+        />
+        <Image
+          src={ReddishLogoOnly}
+          alt="logo"
+          width={40}
+          height={40}
+          className="block md:hidden"
+        />
+      </div>
+
+      {/* Right side */}
+      <div>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+        <SignedOut>
+          <Button asChild variant="outline">
+            <SignInButton mode="modal" />
+          </Button>
+        </SignedOut>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
